c8132750a4b91d823dcdb02cde5432adc9d7db9a
jaqubm <jaqobgm@gmail.com>
2026-05-12 22:20:10 +0200

    Restart of the idea

--- lib/supabase/client.ts
+++
@@ -1,8 +0,0 @@
-import { createBrowserClient } from '@supabase/ssr'
-
-export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_justmove_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_justmove_SUPABASE_ANON_KEY!,
-  )
-}
